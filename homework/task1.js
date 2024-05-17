02ab70a8b3e531d783fbc0f33cb9d7009338984e
grus4 <inter84ruslan@gmail.com>
2024-05-17 15:30:34 +0300

    Added task1.js file

---
+++ homework/task1.js
@@ -0,0 +1,17 @@
+let avarageGrade = undefined;
+
+if (avarageGrade == null || avarageGrade == undefined) {
+    console.log("Помилка в визначенні середньої оцінки - пусте значення");
+} else if (avarageGrade < 60) {
+    console.log("Незадовільно");
+} else if (avarageGrade >= 60 && avarageGrade <= 70) {
+    console.log("Задовільно");
+} else if (avarageGrade > 70 && avarageGrade <= 80) {
+    console.log("Добре");
+} else if (avarageGrade > 80 && avarageGrade <= 90) {
+    console.log("Дуже добре");
+} else if (avarageGrade > 90 && avarageGrade <= 100) {
+    console.log("Відмінно");
+} else {
+    console.log("Невірна оцінка");
+}
