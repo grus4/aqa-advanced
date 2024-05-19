2dde8975d6fecc2fdc6524af852bab49128745d9
grus4 <inter84ruslan@gmail.com>
2024-05-19 14:36:45 +0300

    added task3.js, added task4.js

---
+++ homework/task3.js
@@ -0,0 +1,6 @@
+const number = 4;
+
+for (i = 1; i <= 10; i++) {
+  let result = number * i;
+  console.log(`${number} x ${i} =  ${result}`);
+}
---
+++ homework/task4.js
@@ -0,0 +1,8 @@
+const number = 6;
+let i = 1;
+
+while (i <= 10) {
+  let result = number * i;
+  console.log(`${number} x ${i} = ${result}`);
+  i++;
+}
