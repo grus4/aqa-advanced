497bcce60eb4aba9377c3ef05d789af2c64900be
grus4 <inter84ruslan@gmail.com>
2024-05-27 15:13:30 +0300

    Modified task1.js and added task3.js files

--- homewrok_advanced_functions/task1.js
+++ homewrok_advanced_functions/task1.js
@@ -1,3 +1,30 @@
+//The First version of the callback is using arrow functions
+const handleEvenNumber = () => {
+  console.log(`Number is even`);
+};
+
+const handleOddNumber = () => {
+  console.log(`Number is odd`);
+};
+
+const handleNumbers = (number, handleEvenCallBack, handleOddCallBack) => {
+  if (number % 2 === 0) {
+    handleEvenCallBack();
+  } else {
+    handleOddCallBack();
+  }
+};
+
+const numberToCheck = 8;
+
+handleNumbers(numberToCheck, handleEvenNumber, handleOddNumber);
+
+/* The second version of the callback function
+is usdng a functional expression
+but the message hows the exact number(which was passed to the handleNum (HOF) function)
+ in the console
+*/
+
 const handleEven = function () {
   console.log(`Number: ${this.number} is even`);
 };
